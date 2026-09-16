8d82e88008d58ae91d94af92a0c709629abe56ef
cuentapro11 <beboagua9@gmail.com>
2026-09-16 11:09:48 -0400

    Update YouTube video ID and playlist in JAVAB.js

--- JAVAB.js
+++ JAVAB.js
@@ -138,7 +138,7 @@ function initializeYouTubePlayer() {
     player = new YT.Player('youtube-player', {
         height: '1',
         width: '1',
-        videoId: '2kIbq2igNUo',
+        videoId: 'vwp1yxtcD7I',
         playerVars: {
             autoplay: 0,
             controls: 0,
@@ -150,7 +150,7 @@ function initializeYouTubePlayer() {
             rel: 0,
             showinfo: 0,
             iv_load_policy: 3,
-            playlist: '2kIbq2igNUo'
+            playlist: 'vwp1yxtcD7I'
         },
         events: {
             'onReady': onPlayerReady,
